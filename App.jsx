7701948ad18d30809cc7ac99a06408d8d6b35696
Yuki-KG <yuki@kawagishi.com>
2022-03-19 23:04:48 +0900

    Created the components folder, and modified App.jsx to style the title bar.

--- App.jsx
+++ App.jsx
@@ -5,6 +5,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarTitle}>Gematria</Text>
+        </View>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
@@ -15,8 +20,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8',
+  },
+  appBar: {
+    backgroundColor: '#467FD3',
+    width: '100%',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  appBarInner: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appBarTitle: {
+    color: '#ffffff',
+    fontSize: 22,
+    fontWeight: 'bold',
+    lineHeight: 32,
+    marginBottom: 8,
   },
 });
